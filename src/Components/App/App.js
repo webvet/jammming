c38edab594c7fd456658594422a733adeda27635
webvet <webvetservices@gmail.com>
2022-03-24 23:56:23 +0000

    Refactor relative path

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
-import './App/App';
+import './App';
 import './Playlist/Playlist';
 import './SearchResults/SearchResults';
 
 class App extends React.Component {
   render(
     <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <!-- Add a SearchBar component -->
-    <div className="App-playlist">
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+      <!-- Add a SearchBar component -->
+        <div className="App-playlist">
       <!-- Add a SearchResults component -->
       <!-- Add a Playlist component -->
+        </div>
+        </div>
     </div>
-  </div>
-</div>
   )
 }
 
