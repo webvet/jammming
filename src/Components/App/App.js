9583a0dc49a0ac36364e3bcf644c7a2f4be38573
webvet <webvetservices@gmail.com>
2022-03-25 01:10:03 +0000

    Tidy up some deleted files and renames

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,44 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+/*import logo from './logo.svg';*/
 import './App';
-import './SearchBar/SearchBar';
-import './SearchResults/SearchResults';
-import './Playlist/Playlist';
+import './App.css';
+// import './SearchBar/SearchBar';
+// import './SearchResults/SearchResults';
+// import './Playlist/Playlist';
 
 class App extends React.Component {
-  render(
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
-      <!-- Add a SearchBar component -->
-        <div className="App-playlist">
-      <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component -->
-        </div>
-        </div>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+        {/*<SearchBar />*/}
+          <div className="App-playlist">
+        {/*<SearchResults />*/}
+        {/*<Playlist />*/}
+          </div>
+          </div>
+      </div>
+    )
+  }
 }
-
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 export default App;
